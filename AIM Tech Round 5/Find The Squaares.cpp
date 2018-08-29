cb470aff08a307008f75e35bf5beeebd80a1e621
AshishGarg9631 <42091263+AshishGarg9631@users.noreply.github.com>
2018-08-29 18:05:06 +0530

    Update Find The Squaares.cpp

--- AIM Tech Round 5/Find The Squaares.cpp
+++ AIM Tech Round 5/Find The Squaares.cpp
@@ -1,29 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define inf 1e9
 int main(){
-    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
-    int n,m,l,k,c,i,j;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n,m,i,j,x1=inf,x2=-inf,y1=inf,y2=-inf;
     cin>>n>>m;
     char a[n][m];
-    l=0,k=0,c=0;
-    for(i=1;i<=n;i++)
-        for(j=1;j<=m;j++)
+    for(i=0;i<n;i++)
+        for(j=0;j<m;j++)
             cin>>a[i][j];
-    for(i=1;i<=n;i++){
-        for(j=1;j<=m;j++){
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
             if(a[i][j]=='B'){
-                c++;
-                k=j;
+                y1=min(y1,i);
+                x1=min(x1,j);
+                y2=max(y2,i);
+                x2=max(x2,j);
             }
         }
-        if(c>0){
-            l=i;
-            break;
-        }
     }
-    if(c==1)
-        cout<<l<<" "<<k<<"\n";
-    else
-        cout<<l+c/2<<" "<<k-c/2<<"\n";
+    cout<<(y2+y1)/2 + 1<<" "<<(x2+x1)/2 + 1<<"\n";
     return 0;
 }
